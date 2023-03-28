04785334628fe41a43096498eb21e1568a1ae74e
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-03-28 04:34:00 +0000

    Bug is with react refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
   entry: './index.js',
-  plugins: [new HtmlWebpackPlugin()],
-  mode: 'development'
+  plugins: [new HtmlWebpackPlugin(), new ReactRefreshWebpackPlugin()],
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              plugins: [require.resolve('react-refresh/babel')]
+            },
+          },
+        ],
+      },
+    ],
+  },
 }
